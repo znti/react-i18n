5e110f55c68b0e84cad162436874f2611ba363c3
Zanetti <znti.rafael@gmail.com>
2019-10-12 19:42:49 -0300

    Adjust provider with props for v1.1.0+

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import LanguageProvider from 'react-language-kit';
 export default function App() {
 	return (
 		<LanguageProvider
+			//v1.0.0
 			language={'pt'}
 			languages={['en', 'pt']}
+			//v1.1.0 and above
+			activeLanguage={'pt'}
+			availableLanguages={['en', 'pt']}
 		>
 			<RetrocompatPage />
 		</LanguageProvider>
