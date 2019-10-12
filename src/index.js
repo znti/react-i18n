aece285f3246f93ed185338dec78aeafcd519740
Zanetti <znti.rafael@gmail.com>
2019-10-11 23:11:41 -0300

    Remove unused function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React, { createContext, useContext, useReducer } from 'react';
 const LanguageContext = createContext();
 
 const reducer = (state, language) => {
-	console.log('State', state, 'and lang', language);
 	if(state.options.includes(language)) {
 		return { ...state, language };
 	}
@@ -22,21 +21,4 @@ export default function LanguageProvider({children, languages, language}) {
 }
 
 export const useLanguage = () => useContext(LanguageContext);
-
-export const chooseStrings = (options) => {
-	const [ {language} ] = LanguageContext._currentValue;
-
-	const chosenStrings = options[language];
-
-	if(chosenStrings) {
-		return chosenStrings;
-	}
-
-	// Choose the first available option as default
-	if(Object.keys(options).length > 0) {
-		return options[Object.keys(options)[0]];
-	}
-
-	return {}
-}
 
